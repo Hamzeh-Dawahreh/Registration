bf223f78a2fb713d3fb8189df3e18931e7461c49
Dawahreh <hamza.mohmmad99@yahoo.com>
2023-04-04 21:54:50 +0300

    Added new file

--- app.js
+++ app.js
@@ -1,36 +1,53 @@
-let array = JSON.parse(localStorage.getItem("info")) || [];
-render();
+let array =[];
 
 let form = document.getElementById("studentsForm");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  let fullName = event.target.fname.value;
-  let birth = event.target.birth.value;
-  let gender = event.target.gender.value;
-  let phone = event.target.phone.value;
-  let grade = event.target.grade.value;
-  let userimage = event.target.userimage.value;
+  let username = event.target.username.value;
+  let password = event.target.password.value;
+  let email = event.target.email.value;
+  
 
-  let newStudent = new Student(
-    fullName,
-    birth,
-    gender,
-    phone,
-    grade,
-    userimage
-  );
-  array.push(newStudent);
-  let JSONarray = JSON.stringify(array);
-  localStorage.setItem("info", JSONarray);
-
+  if( usernameValidation(username) &&  passwordValidation(password) && emailValidation(email)){
+    array.push(username);
+    sessionStorage.setItem('array',JSON.stringify(array) );
+  }
   form.reset();
 });
 
-function Student(name, dateOfBirth, gender, phone, grade, userimage) {
-  this.fullName = name;
-  this.dateOfBirth = dateOfBirth;
-  this.gender = gender;
-  this.phone = phone;
-  this.grade = grade;
-  this.userimage = userimage;
+function usernameValidation(username){
+  const usernameRegex = /^\S+$/;
+
+  if (usernameRegex.test(username)) {
+    return true;
+  } else {
+    return false;
+  }
 }
+
+function passwordValidation(password){
+  const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]).{8,}$/;
+
+  if (passwordRegex.test(password)) {
+    return true;
+  } else {
+    alert("Insert more than 8 characters, with at least 1 number, uppercase, and special characters.")
+
+    return false;
+  }
+}
+
+function emailValidation(email) {
+  
+  let re = /\S+@\S+\.\S+/;
+  if (emailValidation(email)) {
+    alert("Email is valid");
+  } else {
+    alert("Email is invalid");
+  }
+  return re.test(email);
+
+
+}
+
+ 
