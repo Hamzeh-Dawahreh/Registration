580a03d7cf311ef07436a9a2c0654e9d551f34dc
Dawahreh <HAMZA.MOHMMAD99@YAHOO.COM>
2023-04-11 22:47:17 +0300

    Updated

--- app.js
+++ app.js
@@ -1,54 +1,121 @@
-let array = sessionStorage.array || [];
+// Step 1: Create a Student constructor function
+function Register(Username, Email, Password, Phone) {
+  this.Username = Username;
+  this.Email = Email;
+  this.Password = Password;
+  this.Phone = Phone;
+}
 
-let form = document.getElementById("studentsForm");
-form.addEventListener("submit", (event) => {
+const myForm = document.getElementById("ffrom");
+
+let registerList = JSON.parse(sessionStorage.getItem("registerList")) || [];
+
+myForm.addEventListener("submit", function (event) {
   event.preventDefault();
-  let username = event.target.username.value;
-  let password = event.target.password.value;
-  let email = event.target.email.value;
-  
+  const formData = new FormData(myForm);
 
-  if( usernameValidation(username) &&  passwordValidation(password) && emailValidation(email)){
-    array.push(username);
-    sessionStorage.setItem('array',JSON.stringify(array) );
+  function validateForm(formData) {
+    const usernameRegex = /^\S*$/;
+    const passwordRegex =
+      /^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[A-Z])[a-zA-Z0-9!@#$%^&*]{8,}$/;
+
+    const emailRegex =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    const phoneRegex = /^07\d{8}$/;
+    const errorBox = document.getElementById("error-box");
+    if (!usernameRegex.test(formData.get("Username"))) {
+      errorBox.classList.remove("d-none");
+      errorBox.innerHTML = "Username cannot contain spaces";
+      return false;
+    }
+    if (registerList.includes(formData.get("Username"))) {
+      errorBox.classList.remove("d-none");
+      errorBox.innerHTML =
+        "Username already exists, please choose a different one";
+      return false;
+    }
+
+    if (!passwordRegex.test(formData.get("Password"))) {
+      errorBox.classList.remove("d-none");
+      errorBox.innerHTML =
+        "Password must contain at least 8 characters, 1 number, 1 uppercase letter, and 1 special character";
+      return false;
+    }
+
+    if (!emailRegex.test(formData.get("Email"))) {
+      errorBox.classList.remove("d-none");
+      errorBox.innerHTML = "Invalid email address";
+      return false;
+    }
+
+    if (!phoneRegex.test(formData.get("Phone"))) {
+      errorBox.classList.remove("d-none");
+      errorBox.innerHTML =
+        "Phone number must start with 07 and consist of 10 digits";
+      return false;
+    }
+    return true;
   }
-  // form.reset();
+
+  // find is a method to search through the array and find an object with a Username property that matches the value.
+  const existingRegister = registerList.find(
+    (existsAlready) => existsAlready.Username === formData.get("Username")
+  );
+  if (existingRegister) {
+    errorBox.classList.remove("d-none");
+    errorBox.innerHTML =
+      "Username already exists, please choose a different one";
+    return false;
+  }
+
+  if (validateForm(formData)) {
+    let newRegister = new Register(
+      formData.get("Username"),
+      formData.get("Email"),
+      formData.get("Password"),
+      formData.get("Phone")
+    );
+
+    registerList.push(newRegister);
+    //stores the array in the sesstion Storage
+    sessionStorage.setItem("registerList", JSON.stringify(registerList));
+    renderRegister();
+  }
 });
 
-function usernameValidation(username){
-  const usernameRegex = /^\S+$/;
+function renderRegister() {
+  for (const register of registerList) {
+    const card = document.getElementById("card");
+    card.classList.remove("d-none");
+    const cont = document.createElement("div");
+    card.appendChild(cont);
 
-  if (usernameRegex.test(username)) {
-    return true;
-  } else {
-    return false;
+    const ul = document.createElement("ul");
+    cont.appendChild(ul);
+
+    const username = document.createElement("li");
+    username.innerHTML = register.Username;
+    ul.appendChild(username);
+
+    const email = document.createElement("li");
+    email.innerHTML = `Email: ${register.Email}`;
+    ul.appendChild(email);
+
+    const phone = document.createElement("li");
+    phone.innerHTML = `Phone: ${register.Phone}`;
+    ul.appendChild(phone);
   }
 }
-
-function passwordValidation(password){
-  const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]).{8,}$/;
-
-  if (passwordRegex.test(password)) {
-    return true;
-  } else {
-    alert("Insert more than 8 characters, with at least 1 number, uppercase, and special characters.")
-
+function validateForm(formData) {
+  // other validation checks
+  if (!usernameRegex.test(formData.get("Username"))) {
+    const errorBox = document.getElementById("error-box");
+    errorBox.innerHTML = "Username cannot contain spaces";
+    errorBox.style.backgroundColor = "red"; // set the background color to red
     return false;
   }
+  // other validation checks
 }
 
-function emailValidation(email) {
-  let re = /\S+@\S+\.\S+/;
-  if (!re.test(email)) {
-    alert("Email is invalid. Please enter a valid email address.");
-    return false;
-  }
-  return true;
-}
-
-
-
-
-
-
- 
+window.onload = renderRegister;
